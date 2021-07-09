bc366462d7026d092373e07ffc06870262683560
jakeogh <github.com@v6y.net>
2021-07-09 12:25:48 -0700

    Initial commit
    This commit is the result of 37 individual commits made over 1 year, 2 months, 28 days.

---
+++ timetool/timetool.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
+# pylint: disable=C0114  #      Missing module docstring (missing-module-docstring)
+# pylint: disable=fixme                           # [W0511] todo is encouraged
+# pylint: disable=line-too-long                   # [C0301]
+# pylint: disable=too-many-instance-attributes    # [R0902]
+# pylint: disable=too-many-lines                  # [C0302] too many lines in module
+# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
+# pylint: disable=too-many-return-statements      # [R0911]
+# pylint: disable=too-many-branches               # [R0912]
+# pylint: disable=too-many-statements             # [R0915]
+# pylint: disable=too-many-arguments              # [R0913]
+# pylint: disable=too-many-nested-blocks          # [R1702]
+# pylint: disable=too-many-locals                 # [R0914]
+# pylint: disable=too-few-public-methods          # [R0903]
+# pylint: disable=no-member                       # [E1101] no member for base
+# pylint: disable=attribute-defined-outside-init  # [W0201]
+# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
+
+from __future__ import annotations
+
+import errno
+import os
+import time
+from collections.abc import Sequence
+from datetime import datetime
+from decimal import Decimal
+from functools import wraps
+from pathlib import Path
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
+
+import click
+import dateparser
+from asserttool import ic
+from click_auto_help import AHGroup
+from clicktool import click_add_options
+from clicktool import click_global_options
+from clicktool import tv
+from eprint import eprint
+from humanize import naturaldelta
+from humanize import naturaltime
+from humanize import precisedelta
+from mptool import output
+from unmp import unmp
+
+# from unitcalc import convert
+
+signal(SIGPIPE, SIG_DFL)
+
+
+def get_timestamp():
+    stamp = str("%.22f" % time.time())
+    return stamp
+
+
+def timestamp_to_epoch(date_time: str):
+    # date_time = '2016-03-14T18:54:56.1942132'.split('.')[0]
+    date_time = date_time.split(".")[0]
+    pattern = "%Y-%m-%dT%H:%M:%S"
+    epoch = int(time.mktime(time.strptime(date_time, pattern)))
+    return epoch
+
+
+def timeit(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        eprint(
+            "func:%r args:[%r, %r] took: %2.4f sec" % (f.__name__, args, kw, te - ts)
+        )
+        return result
+
+    return timed
+
+
+def get_mtime(infile):
+    mtime = os.lstat(infile).st_mtime  # does not follow symlinks
+    return mtime
+
+
+def get_amtime(infile):
+    try:
+        infile_stat = os.lstat(infile)
+    except TypeError:
+        infile_stat = os.lstat(infile.fileno())
+    amtime = (infile_stat.st_atime_ns, infile_stat.st_mtime_ns)
+    return amtime
+
+
+def update_mtime_if_older(
+    *,
+    path: Path,
+    mtime: tuple[int, int],
+    verbose: bool | int | float,
+):
+    assert isinstance(mtime, tuple)
+    assert isinstance(mtime[0], int)
+    assert isinstance(mtime[1], int)
+    current_mtime = get_amtime(path)
+    if current_mtime[1] > mtime[1]:
+        if verbose:
+            eprint(
+                "{} old: {} new: {}".format(path.as_posix(), current_mtime[1], mtime[1])
+            )
+        os.utime(path, ns=mtime, follow_symlinks=False)
+
+
+def timeout(seconds, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+
+# in epochfilter
+# def human_date_to_timestamp(date):
+#    dt = dateparser(date)
+#    return dt.timestamp()
+
+
+def seconds_duration_to_human_readable(seconds, *, ago: bool, short: bool = True):
+    if seconds is None:
+        return None
+    seconds = float(seconds)
+    if ago:
+        result = naturaltime(seconds)
+    else:
+        # result = naturaldelta(seconds)
+        result = precisedelta(seconds, minimum_unit="minutes")
+
+    if short:
+        result = result.replace(" seconds", "s")
+        result = result.replace("a second", "1s")
+
+        result = result.replace(" minutes", "min")
+        result = result.replace("a minute", "1min")
+
+        result = result.replace(" hours", "hr")
+        result = result.replace("an hour", "1hr")
+
+        result = result.replace(" days", "days")
+        result = result.replace("a day", "1day")
+
+        result = result.replace(" months", "mo")
+        result = result.replace("a month", "1mo")
+
+        result = result.replace(" years", "yrs")
+        result = result.replace("a year", "1yr")
+        result = result.replace("1 year", "1yr")
+
+        result = result.replace(" ago", "_ago")
+        result = result.replace(", ", ",")
+    return result
+
+
+def human_date_to_timestamp(date):
+    dt = dateparser.parse(date)
+    timestamp = dt.timestamp()
+    return Decimal(str(timestamp))
+
+
+def timestamp_to_human_date(timestamp: str):
+    # timestamp = Decimal(str(timestamp))
+    human_date = datetime.fromtimestamp(float(timestamp)).strftime(
+        "%Y-%m-%d %H:%M:%S %Z"
+    )
+    return human_date
+
+
+def day_month_year_to_datetime(*, day: int, month: int, year: int):
+    return datetime(year=year, month=month, day=day)
+
+
+def humanize_history_dict(history):
+    human_history = {}
+    human_history["time_unchanged"] = seconds_duration_to_human_readable(
+        history["time_unchanged"], ago=False
+    )
+    human_history["age"] = seconds_duration_to_human_readable(history["age"], ago=True)
+    return human_history
+
+
+@click.group(no_args_is_help=True, cls=AHGroup)
+@click_add_options(click_global_options)
+@click.pass_context
+def cli(
+    ctx,
+    verbose: bool | int | float,
+    verbose_inf: bool,
+    dict_output: bool,
+):
+
+    ctx.ensure_object(dict)
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
+
+
+@cli.command("timestamp-to-human-duration")
+@click.argument("timestamps", type=str, nargs=-1)
+@click_add_options(click_global_options)
+@click.pass_context
+def _timestamp_to_human_duration(
+    ctx,
+    timestamps: Sequence[str],
+    verbose: bool | int | float,
+    verbose_inf: bool,
+    dict_output: bool,
+) -> None:
+
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
+
+    if timestamps:
+        iterator = timestamps
+    else:
+        iterator = unmp(
+            valid_types=[
+                str,
+                int,
+            ],
+            verbose=verbose,
+        )
+    del timestamps
+
+    index = 0
+    for index, _timestamp in enumerate(iterator):
+        if verbose:
+            ic(index, _timestamp)
+
+        _timestamp = int(_timestamp)
+        human_duration = seconds_duration_to_human_readable(
+            _timestamp, ago=False, short=False
+        )
+        ic(human_duration)
+
+        output(
+            human_duration,
+            reason=None,
+            dict_output=dict_output,
+            tty=tty,
+            verbose=verbose,
+        )
+
+
+@cli.command("timestamp-to-human-date")
+@click.argument("timestamps", type=str, nargs=-1)
+@click_add_options(click_global_options)
+@click.pass_context
+def _timestamp_to_human_date(
+    ctx,
+    timestamps: Sequence[str],
+    verbose: bool | int | float,
+    verbose_inf: bool,
+    dict_output: bool,
+) -> None:
+
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
+
+    if timestamps:
+        iterator = timestamps
+    else:
+        iterator = unmp(
+            valid_types=[
+                str,
+                int,
+            ],
+            verbose=verbose,
+        )
+    del timestamps
+
+    index = 0
+    for index, _timestamp in enumerate(iterator):
+        if verbose:
+            ic(index, _timestamp)
+
+        _timestamp = int(_timestamp)
+        human_timestamp = timestamp_to_human_date(_timestamp)
+
+        output(
+            human_timestamp,
+            reason=None,
+            dict_output=dict_output,
+            tty=tty,
+            verbose=verbose,
+        )
