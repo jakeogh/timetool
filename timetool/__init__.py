5fc09ecb23e2952edc83b6ef1d37e106d85d9131
jakeogh <github.com@v6y.net>
2023-09-21 15:32:30 -0700

    auto-commit

--- timetool/__init__.py
+++ timetool/__init__.py
@@ -1,6 +1,5 @@
 from .timetool import get_amtime
 from .timetool import get_mtime
-from .timetool import get_timestamp
 from .timetool import human_date_to_timestamp
 from .timetool import humanize_history_dict
 from .timetool import seconds_duration_to_human_readable
