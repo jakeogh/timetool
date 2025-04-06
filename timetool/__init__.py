d6ce6f5303836924d372243f1f45b9832364b314
jakeogh <github.com@v6y.net>
2025-04-06 10:33:56 -0700

    auto-commit

--- timetool/__init__.py
+++ timetool/__init__.py
@@ -5,6 +5,7 @@ isort:skip_file
 from .timetool import get_amtime as get_amtime
 from .timetool import get_mtime as get_mtime
 from .timetool import human_date_to_timestamp as human_date_to_timestamp
+from .timetool import human_date_to_datetime as human_date_to_datetime
 from .timetool import humanize_history_dict as humanize_history_dict
 from .timetool import (
     seconds_duration_to_human_readable as seconds_duration_to_human_readable,
