b34661bf3eac506f695a7589e4f134d0cf0ad120
jakeogh <github.com@v6y.net>
2025-10-18 14:01:42 -0700

    auto-commit

--- timetool/timetool.py
+++ timetool/timetool.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=missing-param-doc               # [W9015]
-# pylint: disable=missing-module-docstring        # [C0114]
-# pylint: disable=fixme                           # [W0511] todo encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-many-public-methods         # [R0904]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 from __future__ import annotations
 
 import errno
@@ -77,7 +57,6 @@ def timeout(seconds, error_message=os.strerror(errno.ETIME)):
 
 
 def timestamp_to_epoch(date_time: str):
-    # date_time = '2016-03-14T18:54:56.1942132'.split('.')[0]
     date_time = date_time.split(".")[0]
     pattern = "%Y-%m-%dT%H:%M:%S"
     epoch = int(time.mktime(time.strptime(date_time, pattern)))
@@ -195,7 +174,12 @@ def timestamp_to_human_date(timestamp: str):
     return human_date.strip()
 
 
-def day_month_year_to_datetime(*, day: int, month: int, year: int):
+def day_month_year_to_datetime(
+    *,
+    day: int,
+    month: int,
+    year: int,
+):
     return datetime(year=year, month=month, day=day)
 
 
